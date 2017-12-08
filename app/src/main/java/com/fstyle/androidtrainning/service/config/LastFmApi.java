b2f0924c911e0daf235e1b26d7a71368508ee1d0
vanchuong119 <vanchuong119@gmail.com>
2017-12-08 19:34:28 +0700

    Chuong init

---
+++ app/src/main/java/com/fstyle/androidtrainning/service/config/LastFmApi.java
@@ -0,0 +1,14 @@
+package com.fstyle.androidtrainning.service.config;
+
+/**
+ * Created by Administrator on 12/08/17.
+ */
+
+public interface LastFmApi {
+
+    //TODO vi du api
+    //tu ket noi det github search va tu dong tao object. tra ve object
+    //    @GET("/search/users")
+    //    Call<SearchGithubUserResponse> seacrhGithubUser(@Query("per_page") int limit, @Query
+    // ("q") String keyword);
+}
