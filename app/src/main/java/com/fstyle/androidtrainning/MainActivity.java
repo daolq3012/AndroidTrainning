d5944a7f71acc4dfb0fe2d6bc506686f22108ea3
truyenz <tdtruyen92lmq@gmail.com>
2017-12-11 19:05:42 +0700

    Fix warning

--- app/src/main/java/com/fstyle/androidtrainning/MainActivity.java
+++ app/src/main/java/com/fstyle/androidtrainning/MainActivity.java
@@ -2,8 +2,6 @@ package com.fstyle.androidtrainning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("Thang", "AAA");
-        Toast.makeText(this,"Test Conflict",Toast.LENGTH_LONG).show();
-        Toast.makeText(this, "Home fragment !", Toast.LENGTH_LONG).show();
     }
 }
