967448ed5e569f7e1f7040f12687d36c5d9bee1b
truyenz <tdtruyen92lmq@gmail.com>
2017-12-12 15:18:18 +0700

    Init MoviesApi Complete

--- app/src/main/java/com/fstyle/androidtrainning/MainActivity.java
+++ app/src/main/java/com/fstyle/androidtrainning/MainActivity.java
@@ -2,6 +2,14 @@ package com.fstyle.androidtrainning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.fstyle.androidtrainning.data.model.Movie;
+import com.fstyle.androidtrainning.data.service.config.MoviesApi;
+import com.fstyle.androidtrainning.data.service.response.GetListUpcomingResponse;
+import com.fstyle.androidtrainning.utils.Constant;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MoviesApi api = MainApplication.getMoviesApi();
+        api.getListUpcomingMovies(Constant.API_KEY,Constant.LANGUAGE,1).enqueue(new Callback<GetListUpcomingResponse>() {
+            @Override
+            public void onResponse(Call<GetListUpcomingResponse> call,
+                    Response<GetListUpcomingResponse> response) {
+                List<Movie> movieList = response.body().getResults();
+            }
+
+            @Override
+            public void onFailure(Call<GetListUpcomingResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/fstyle/androidtrainning/utils/Constant.java
@@ -0,0 +1,10 @@
+package com.fstyle.androidtrainning.utils;
+
+/**
+ * Created by Admin on 12/12/17.
+ */
+
+public class Constant {
+    public static final String API_KEY = "81926142a0ec9e95744652262dc8ec1c";
+    public static final String LANGUAGE = "en-US";
+}
